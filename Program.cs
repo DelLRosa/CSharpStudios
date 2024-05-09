37cac89e6410f9ff9d53b446c70cb9bc260ed2ba
Delaney Green <delaney.rosa1@gmail.com>
2024-05-09 12:20:30 -0500

    first commit, first studio

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿
+System.Console.WriteLine("enter the radius here: ");
+
+double radius=Convert.ToDouble(Console.ReadLine());
+// System.Console.WriteLine(radius);
+// System.Console.WriteLine(radius.GetType());
+
+double area =Math.PI * (radius*radius);
+double c = 2*Math.PI*radius;
+double diameter = 2*radius;
+
+System.Console.WriteLine("The area is "+area +", the circumference is "+c+" and the diameter is "+diameter);
+System.Console.WriteLine("Let's go on a road Trip!! How many miles per gallon does your car get? ");
+double mpg = Convert.ToDouble(Console.ReadLine());
+double gallons = c/mpg;
+
+System.Console.WriteLine("You will use "+gallons+" gallons to go around the outside of our circle");
